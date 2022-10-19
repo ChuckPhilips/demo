8ed952e7803021b2fd2101f2e11b9939c34a4f41
Filip Cuic <filip.cuic@vicert.com>
2022-10-19 21:38:42 +0200

    Go

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -63,7 +63,7 @@ variable "global_tags" {
 locals {
   postfix         = "${var.environment}-${data.aws_caller_identity.current.account_id}"
   container_image = "454624638483.dkr.ecr.us-east-2.amazonaws.com/backend:${var.image_tag}"
-  proxy_image = "454624638483.dkr.ecr.us-east-2.amazonaws.com/proxy:latest"
+  proxy_image     = "454624638483.dkr.ecr.us-east-2.amazonaws.com/proxy:latest"
 
   tags = merge(var.global_tags,
     {
@@ -79,21 +79,21 @@ module "vpc" {
 }
 
 module "ecs" {
-  source              = "../../modules/ecs"
-  postfix_in          = "dev"
-  container_image_in  = local.container_image
-  proxy_image_in      = local.proxy_image
-  vpc_id_in           = module.vpc.id
-  subnets_in          = module.vpc.private_subnets_ids
-  target_group_arn_in = module.loadbalancer.target_group_arn
-  nodejs_port_in      = var.nodejs_port
-  proxy_container_port_in = var.proxy_container_port
+  source                   = "../../modules/ecs"
+  postfix_in               = "dev"
+  container_image_in       = local.container_image
+  proxy_container_image_in = local.proxy_image
+  vpc_id_in                = module.vpc.id
+  subnets_in               = module.vpc.private_subnets_ids
+  target_group_arn_in      = module.loadbalancer.target_group_arn
+  nodejs_port_in           = var.nodejs_port
+  proxy_container_port_in  = var.proxy_container_port
 }
 
 module "loadbalancer" {
-  source         = "../../modules/loadbalancer"
-  vpc_id_in      = module.vpc.id
-  postfix_in     = "dev"
-  subnets_in     = module.vpc.public_subnets_ids
+  source                = "../../modules/loadbalancer"
+  vpc_id_in             = module.vpc.id
+  postfix_in            = "dev"
+  subnets_in            = module.vpc.public_subnets_ids
   backend_proxy_port_in = var.proxy_container_port
 }
