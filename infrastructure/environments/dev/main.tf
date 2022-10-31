2be20b3b987865a5dbff1fae3ee6ca781eddb42c
Filip Cuic <filip.cuic@vicert.com>
2022-10-31 15:41:20 +0100

    Reverting

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -41,43 +41,43 @@ module "account" {
 }
 ###
 
-module "vpc" {
-  source              = "../../modules/vpc"
-  cidr_block_in       = var.cidr_block
-  postfix_in          = var.environment_name
-  environment_name_in = var.environment_name
-}
-
-module "loadbalancer" {
-  source                = "../../modules/loadbalancer"
-  vpc_id_in             = module.vpc.id
-  subnets_in            = module.vpc.public_subnets_ids
-  backend_proxy_port_in = var.backend_proxy_container_port
-  dns_zone_id_in        = module.account.zone_id
-  environment_name_in   = var.environment_name
-}
-
-module "ecs" {
-  source              = "../../modules/ecs"
-  environment_name_in = var.environment_name
-}
-
-module "backend" {
-  source                   = "../../modules/backend"
-  cluster_name_in          = module.ecs.name
-  app_container_image_in   = local.app_image
-  app_container_name_in    = "app"
-  app_container_port_in    = var.backend_app_container_port
-  vpc_id_in                = module.vpc.id
-  subnets_in               = module.vpc.private_subnets_ids
-  target_group_arn_in      = module.loadbalancer.target_group_arn
-  proxy_container_port_in  = var.backend_proxy_container_port
-  proxy_container_name_in  = "nginx"
-  proxy_container_image_in = local.proxy_image
-  environment_name_in      = var.environment_name
-}
-
-module "frontend" {
-  source              = "../../modules/frontend"
-  environment_name_in = var.environment_name
-}
+#module "vpc" {
+#  source              = "../../modules/vpc"
+#  cidr_block_in       = var.cidr_block
+#  postfix_in          = var.environment_name
+#  environment_name_in = var.environment_name
+#}
+#
+#module "loadbalancer" {
+#  source                = "../../modules/loadbalancer"
+#  vpc_id_in             = module.vpc.id
+#  subnets_in            = module.vpc.public_subnets_ids
+#  backend_proxy_port_in = var.backend_proxy_container_port
+#  dns_zone_id_in        = module.account.zone_id
+#  environment_name_in   = var.environment_name
+#}
+#
+#module "ecs" {
+#  source              = "../../modules/ecs"
+#  environment_name_in = var.environment_name
+#}
+#
+#module "backend" {
+#  source                   = "../../modules/backend"
+#  cluster_name_in          = module.ecs.name
+#  app_container_image_in   = local.app_image
+#  app_container_name_in    = "app"
+#  app_container_port_in    = var.backend_app_container_port
+#  vpc_id_in                = module.vpc.id
+#  subnets_in               = module.vpc.private_subnets_ids
+#  target_group_arn_in      = module.loadbalancer.target_group_arn
+#  proxy_container_port_in  = var.backend_proxy_container_port
+#  proxy_container_name_in  = "nginx"
+#  proxy_container_image_in = local.proxy_image
+#  environment_name_in      = var.environment_name
+#}
+#
+#module "frontend" {
+#  source              = "../../modules/frontend"
+#  environment_name_in = var.environment_name
+#}
